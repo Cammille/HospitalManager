ebf05fab8257198340308546ae13df111dbd0d53
Camille <pic.camille@gmail.com>
2012-11-30 11:05:51 +0100

    Add files

--- picard_bManageHospital/DataAccess/Patient.cs
+++ picard_bManageHospital/DataAccess/Patient.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace picard_bManageHospital.DataAccess
 {
     class Patient
     {
+        public List<Model.Patient> GetListPatient()
+        {
+            try
+            {
+                //return new ServicePatient.ServicePatientClient().GetListPatient().ToList();
+                return null;
 
+            }
+            catch (Exception ex)
+            {
+                //traitement exception ...
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
     }
 }
---
+++ picard_bManageHospital/Model/Patient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace picard_bManageHospital.Model
+{
+    class Patient
+    {
+
+    }
+}
