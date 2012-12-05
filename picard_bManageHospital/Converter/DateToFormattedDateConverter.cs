45bc4df8320d0eaaa54833b86e9daacc409dfaf4
Camille <pic.camille@gmail.com>
2012-12-05 22:53:03 +0100

    l oiel de florian, les fichiers sont add

---
+++ picard_bManageHospital/Converter/DateToFormattedDateConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace picard_bManageHospital.Converter
+{
+    class DateToFormattedDateConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string pattern = "dd-mm-yyyy";
+            DateTime date = (DateTime)value;
+            return date.ToString(pattern);       
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ picard_bManageHospital/Converter/FirstLetterUpperCaseConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace picard_bManageHospital.Converter
+{
+    class FirstLetterUpperCaseConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            String name = (String)value;
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name.ToLower());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
